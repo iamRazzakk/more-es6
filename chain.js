16816d430744fddb370e3f53d86301b5bc161a32
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-26 12:06:39 +0600

    git commit

---
+++ chain.js
@@ -0,0 +1,25 @@
+// const product = {
+//     count: 5000,
+//     data: [
+//         { id: 1, name: "lenevo leptop", price: 50000 },
+//         { id: 2, name: "MackBook", price: 150000 },
+//         { id: 3, name: "Hp", price: 80000 },
+//         { id: 4, name: "Ace", price: 25000 }
+//     ]
+// }
+// console.log(product.data[1].price);
+
+const user = {
+    id: 5000,
+    name: "Md Abdur Razzak",
+    Adress: {
+        street: {
+            first: "bissocoloni 7 market",
+            secound: "FirozSha colony",
+            third: "Sandwip"
+        },
+        city: "dhaka"
+    }
+}
+// find street secound adress
+console.log(user.Adress.street.secound);
---
+++ map.js
@@ -0,0 +1,7 @@
+// const number = [1, 2, 3, 4, 5]
+// const squearNumber = number.map(number => number * number)
+// console.log(squearNumber);
+
+const number = [1, 2, 3, 4, 5]
+const multiplyByTwo = number.map(number => number * 2)
+console.log(multiplyByTwo);
